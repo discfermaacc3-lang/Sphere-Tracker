530361bf8e3d5d0dd030853da21d18228271a078
discfermaacc3-lang <discferma.acc3@gmail.com>
2026-04-14 09:37:09 +0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
     "public",
   );
   app.use(express.static(frontendPath));
-  app.get("*", (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
